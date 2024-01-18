1ef0a0028f6f4711c1b955d1067f1a9fd3da3b5a
Reelect <rejaealsun@gist.ac.kr>
2024-01-18 23:39:11 +0900

    fix(post): locker assign process fix

--- filetest.py
+++ filetest.py
@@ -1,24 +1,11 @@
-import os
+from fastapi import FastAPI, Depends
 
-import boto3
-import uuid
+from schemas.post import BasePost
+
+fastapi_app = FastAPI(title="DangMooG", debug=True)
 
 
-def upload_file(file: str, folder_name: str):
-    try:
-        # s3 클라이언트 생성
-        s3 = boto3.client(
-            service_name="s3",
-            region_name="ap-northeast-2",
-            aws_access_key_id=os.environ["S3_ACCESS"],
-            aws_secret_access_key=os.environ["S3_SECRET"],
-        )
-    except Exception as e:
-        print(e)
-    else:
-        with open("FILE_NAME", "rb") as f:
-            _, file_extension = os.path.splitext(file)
-            random_file_name = f"{folder_name}/{str(uuid.uuid4()) + file_extension}"
-            s3.upload_fileobj(f, "BUCKET_NAME", "OBJECT_NAME")
-        s3.upload_fileobj(file, "dangmuzi-photo", random_file_name)
-        return "https://dangmuzi-photo.s3.ap-northeast-2.amazonaws.com/"+random_file_name
+@fastapi_app.post("/test")
+def test(req: BasePost):
+
+    return req
