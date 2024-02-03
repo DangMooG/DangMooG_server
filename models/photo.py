207ee3f1e4919e548465654bd57f13f7236d3c2d
Reelect <rejaealsun@gist.ac.kr>
2024-02-04 00:52:28 +0900

    fix(model): MPhoto class message_id foreign key

--- models/photo.py
+++ models/photo.py
@@ -23,7 +23,7 @@ class MPhoto(Base):
     __tablename__ = "m_photo"
     m_photo_id = Column(Integer, nullable=False, autoincrement=True, primary_key=True)
     url = Column(VARCHAR(2000), nullable=False)
-    message_id = Column(Integer, ForeignKey("chat.chat_id"), nullable=False)
+    message_id = Column(Integer, ForeignKey("message.message_id"), nullable=False)
     account_id = Column(Integer, ForeignKey("account.account_id"), nullable=False)
     create_time = Column(TIMESTAMP, nullable=False, server_default=text("CURRENT_TIMESTAMP"))
     mysql_engine = "InnoDB"
