c030b20e86261aa4694e339cec1f35a3c43edec9
Reelect <rejaealsun@gist.ac.kr>
2024-02-25 01:12:00 +0900

    fix(liked): like back error fix

--- models/liked.py
+++ models/liked.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, ForeignKey
+from MySQLdb import TIMESTAMP
+from sqlalchemy import Column, Integer, ForeignKey, text
 
 from core.db import Base
 
@@ -8,5 +9,5 @@ class Liked(Base):
     liked_id = Column(Integer, nullable=False, autoincrement=True, primary_key=True)
     post_id = Column(Integer, ForeignKey("post.post_id"), nullable=False)
     account_id = Column(Integer, ForeignKey("account.account_id"), nullable=False)
-
+    create_time = Column(TIMESTAMP, nullable=False, server_default=text("CURRENT_TIMESTAMP"))
     mysql_engine = "InnoDB"
